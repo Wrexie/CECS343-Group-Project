ee7cbfeae0aec41fe430156fc7312c21618d9ade
drewbaetazar <60937655+drewbaetazar@users.noreply.github.com>
2020-12-04 11:25:00 -0800

    Updated Warehouses

--- CECS343 - Project/src/com/company/Warehouse.java
+++ CECS343 - Project/src/com/company/Warehouse.java
@@ -5,12 +5,28 @@ public class Warehouse {
     private String address;
     private String phone;
 
-    //todo: need warehouse constructor
-    //todo: need setters and getters
-
+    //constructor for warehouse
+    public Warehouse(String name, String address, String phone) {
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
     public void setAddress(String address) {
         this.address = address;
     }
-
-
+    public String getName() {
+        return name;
+    }
+    public String getAddress() {
+        return address;
+    }
+    public String getPhone() {
+        return phone;
+    }
 }
